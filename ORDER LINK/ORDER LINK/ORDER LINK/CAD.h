df6e3fc9257adb5958e1d6ea1d790b566956e94a
Mi2l <119260964+Mi2ll@users.noreply.github.com>
2023-12-01 12:08:30 +0100

    Add files via upload

---
+++ ORDER LINK/ORDER LINK/ORDER LINK/CAD.h
@@ -0,0 +1,21 @@
+#pragma once
+
+namespace NS_Comp_Data
+{
+    ref class CLcad
+    {
+    private:
+        System::String^ sSql;
+        System::String^ sCnx;
+        System::Data::SqlClient::SqlConnection^ oCnx;
+        System::Data::SqlClient::SqlCommand^ oCmd;
+        System::Data::SqlClient::SqlDataAdapter^ oDA;
+        System::Data::DataSet^ oDs;
+
+    public:
+        CLcad(void);
+        System::Data::DataSet^ getRows(System::String^, System::String^);
+        void actionRows(System::String^);
+        void actionRowsForId(System::String^, int);
+    };
+}
