023303ac3084bd54c4c5fe4c07a4d87f090600a6
natroulland <nathan.roulland@viacesi.fr>
2023-12-06 12:01:36 +0100

    apnyana
    ezaeza

---
+++ version du mercredi/Corbeille d'exercices/Corbeille d'exercices/CLCAD.cpp
@@ -0,0 +1,47 @@
+#include "CLCAD.h"
+
+
+NS_Comp_Data::CLcad::CLcad(void)
+{
+	this->sCnx = "Data Source=LAPTOP-4FAP8S0J\\MSSQL_NATHAN;Initial Catalog	= Projet; Integrated Security=True;";
+
+	this->sSql = "Rien";
+
+	this->oCnx = gcnew System::Data::SqlClient::SqlConnection(this->sCnx);
+	this->oCmd = gcnew System::Data::SqlClient::SqlCommand(this->sSql, this->oCnx);
+	this->oDA = gcnew System::Data::SqlClient::SqlDataAdapter();
+	this->oDs = gcnew System::Data::DataSet();
+
+	this->oCmd->CommandType = System::Data::CommandType::Text;
+}
+System::Data::DataSet^ NS_Comp_Data::CLcad::getRows(System::String^ sSql, System::String^ sDataTableName)
+{
+	this->oDs->Clear();
+	this->sSql = sSql;
+	this->oCmd->CommandText = this->sSql;
+	this->oDA->SelectCommand = this->oCmd;
+	this->oDA->Fill(this->oDs, sDataTableName);
+
+	return this->oDs;
+}
+void NS_Comp_Data::CLcad::actionRows(System::String^ sSql)
+{
+	this->sSql = sSql;
+	this->oCmd->CommandText = this->sSql;
+	this->oDA->SelectCommand = this->oCmd;
+	this->oCnx->Open();
+	this->oCmd->ExecuteNonQuery();
+	this->oCnx->Close();
+
+}
+int NS_Comp_Data::CLcad::actionRowsID(System::String^ sSql)
+{
+	int id;
+	this->sSql = sSql;
+	this->oCmd->CommandText = this->sSql;
+	this->oDA->SelectCommand = this->oCmd;
+	this->oCnx->Open();
+	id = System::Convert::ToInt32(this->oCmd->ExecuteScalar());
+	this->oCnx->Close();
+	return id;
+}
